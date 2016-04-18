4558eaf628278ddc9b873eaccb29012cf62f8194
gwang1210 <gary.wang@wustl.edu>
2016-04-18 18:50:54 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -4,9 +4,9 @@ var order = "one za";
 
 // build a function to ask questions
 
-function question(ask, item) {
+function question(item) {
   // your code goes here
-  var answerInput = prompt("do you want" + item);
+  var answerInput = prompt("do you want " + item + "?");
   if ( answerInput.toUpperCase() === "YES" ) {
 order = order + " with " + item + " ,";   
 } else if ( answerInput.toUpperCase() === "NO" ) {
@@ -20,20 +20,20 @@ var pizza = prompt("Suh dude. Welcome to the Za Shack! Would you like to order a
 if ( pizza.toUpperCase() === 'NO' ) {
 order = "no za"
 } else if ( pizza.toUpperCase() === 'YES' ) {
-question("do you want", "cheese");
-question("do you want", "pepperoni");
-question("do you want", "sausage");
-question("do you want", "ground beef");
-question("do you want", "bacon");
-question("do you want", "more bacon");
-question("do you want", "even more bacon");
-question("do you want", "green peppers");
-question("do you want", "onions");
-question("do you want", "spinach");
-question("do you want", "mushrooms");
-question("do you want", "BBQ sauce");
-question("do you want", "tomota sauce");
-question("do you want", "anchovies");
+question("cheese");
+question("pepperoni");
+question("sausage");
+question("ground beef");
+question("bacon");
+question("more bacon");
+question("even more bacon");
+question("green peppers");
+question("onions");
+question("spinach");
+question("mushrooms");
+question("BBQ sauce");
+question("tomota sauce");
+question("anchovies");
 }
 
 
