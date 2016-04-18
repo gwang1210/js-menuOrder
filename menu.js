2a01cb898f91ce471d340520f12c1e952458eaee
gwang1210 <gary.wang@wustl.edu>
2016-04-18 18:45:07 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -16,24 +16,24 @@ order = order + " no " + item  + " ,"
 // call the QUESTION function with the order options
 var pizza = prompt("Suh dude. Welcome to the Za Shack! Would you like to order a za, bruh?");
 
-if ( pizza.toUpperCase() === 'NO' ) {
- order = "no za"
-} else if ( pizza.toUpperCase() === 'YES' ) {
-question("do you want", "cheese");
-question("do you want", "pepperoni");
-question("do you want", "sausage");
-question("do you want", "ground beef");
-question("do you want", "bacon");
-question("do you want", "more bacon");
-question("do you want", "even more bacon");
-question("do you want", "green peppers");
-question("do you want", "onions");
-question("do you want", "spinach");
-question("do you want", "mushrooms");
-question("do you want", "BBQ sauce");
-question("do you want", "tomota sauce");
-question("do you want", "anchovies");
-}
+// if ( pizza.toUpperCase() === 'NO' ) {
+// order = "no za"
+// } else if ( pizza.toUpperCase() === 'YES' ) {
+// question("do you want", "cheese");
+// question("do you want", "pepperoni");
+// question("do you want", "sausage");
+// question("do you want", "ground beef");
+// question("do you want", "bacon");
+// question("do you want", "more bacon");
+// question("do you want", "even more bacon");
+// question("do you want", "green peppers");
+// question("do you want", "onions");
+// question("do you want", "spinach");
+// question("do you want", "mushrooms");
+// question("do you want", "BBQ sauce");
+// question("do you want", "tomota sauce");
+// question("do you want", "anchovies");
+// }
 
 
 // output their order
