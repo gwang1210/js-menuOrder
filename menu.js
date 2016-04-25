def470d2f1604ac5713edd5e9ae6384503e4d755
gwang1210 <gary.wang@wustl.edu>
2016-04-25 18:15:24 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -20,20 +20,38 @@ var pizza = prompt("Suh dude. Welcome to the Za Shack! Would you like to order a
 if ( pizza.toUpperCase() === 'NO' ) {
 order = "no za"
 } else if ( pizza.toUpperCase() === 'YES' ) {
-question("cheese");
-question("pepperoni");
-question("sausage");
-question("ground beef");
-question("bacon");
-question("more bacon");
-question("even more bacon");
-question("green peppers");
-question("onions");
-question("spinach");
-question("mushrooms");
-question("BBQ sauce");
-question("tomota sauce");
-question("anchovies");
+
+//array
+
+var options [
+  "cheese"
+  "pepperoni"
+  "sausage"
+  "bacon"
+  "spinach"
+  "mushrooms"
+  "green peppers"
+  "tomato sauce"
+  ];
+  var i = 0;
+  while (i<options.length) {
+    question(options[i]);
+    i++;
+  }
+// question("cheese");
+// question("pepperoni");
+// question("sausage");
+// question("ground beef");
+// question("bacon");
+// question("more bacon");
+// question("even more bacon");
+// question("green peppers");
+// question("onions");
+// question("spinach");
+// question("mushrooms");
+// question("BBQ sauce");
+// question("tomota sauce");
+// question("anchovies");
 }
 
 
